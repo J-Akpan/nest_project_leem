7bc24c2f2f6de3bef06bc8239a1b454af0a5c2f1
Joe-Akpan <josephakpan316@gmail.com>
2026-02-17 12:32:21 +0100

    entity creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './users/users.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      //to import and inject a  property
+      imports: [],
+      inject: [],
+
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'Akpan123',
+        database: 'leem',
+        entities: [User],
+        synchronize: true, //dont use in production
+
+      })
+
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
